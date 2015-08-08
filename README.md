6aa72e17c8dc06e8566df371c29e1d4db00bb9c7
Aaron Law <aaronlaw@gmail.com>
2015-08-08 19:38:58 +0800

    try to handle path separator on Windows: "\\" and "\"

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ locking files on daily operation
 
 ## Reference:
 
+Python doc:
+https://docs.python.org/3/tutorial/index.html
+Google: python 入門指南
+
 To handle path sep on Windows:
 Google: python path sep on windows
 -> http://stackoverflow.com/questions/16010992/how-to-use-directory-separator-in-both-linux-and-windows
--- test.py
+++ test.py
@@ -6,24 +6,39 @@ As it uses Robocopy, Windows only
 '''
 
 import shutil
+import os # os.path.join()
+
+# to handle path on Windows
+from pathlib import Path, PurePath
 
 folders=['AA0008', 'AA0025']
 # folder='AA0025'
-base_src='H:\\FILES\\2_AUDIT DEPT\\Yr 2015\\AC\\A'
-base_dist='Y:\\test'
+base_src='H:\\FILES\\2_AUDIT DEPT\\Yr 2015\AC\\A'
+base_dist='Y:\\FILES\\2_AUDIT DEPT\\Lock 2015'
 
+print(os.pathsep)
+print(os.path.join('a:', os.sep, 'c') )
+print(os.path.dirname(base_src))
 
-for folder in folders:
-    src = base_src + '\\' + folder
-    dist = base_dist + '\\' + folder
-    try:
-        shutil.copytree(src, dist)
-        #try the move()
-        shutil.move(dist, base_dist + '\\move\\')
-        print('Copy '+ folder +' is ok')
+def replacePathSeperator(path, oldSep, newSep):
+    '''correct path from '\' to '\\' on Windows
+    '''
+    path = path.replace(oldSep, newSep)
+    return path
 
-    except (FileExistsError):
-        print('there is an error on coping ' + folder)
+# print(replacePathSeperator(base_src, '/', os.sep) #os.sep is '\\' on Windows
 
-    finally:
-        pass
+# for folder in folders:
+#     src = base_src + '\\' + folder
+#     dist = base_dist + '\\' + folder
+#     try:
+#         shutil.copytree(src, dist)
+#         #try the move()
+#         shutil.move(dist, base_dist + '\\move\\')
+#         print('Copy '+ folder +' is ok')
+
+#     except (FileExistsError):
+#         print('there is an error on coping ' + folder)
+
+#     finally:
+#         pass
